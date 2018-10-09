5aa9fd3efe4ec3b1a642d57580e2933addada373
Aislan Jefferson <aislanjsb@gmail.com>
2018-10-09 09:51:46 -0300

    Iniciado rascunho do SecondChance

--- lab_mem/page_replacement/python/SecChancePolicy.py
+++ lab_mem/page_replacement/python/SecChancePolicy.py
@@ -9,10 +9,14 @@ class SecChancePolicy(FifoPolicy):
     def evict(self):
         """Deallocates a frame from the physical memory and returns its frameId"""
         item = self.fifo.pop(0)
-        if (item[0]):
+        if (not item[0]):
             return item[1]
         else:
-             item[0] = not item[0]
              self.fifo.append(item)
         """verificar se o self.evict() eh o do FIfoPolicy ou o do SecChancePolicy"""
         return self.evict()
+
+    def access(self, frameId, isWrite):
+        for frame in frames:
+            if(frameId == frame[1]):
+                frameId[0] = True
