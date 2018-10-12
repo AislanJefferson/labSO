6ae285ccb737db951cf4dc0521a0a0377876791d
Filipe Gomes <filipe.lima@ccc.ufcg.edu.br>
2018-10-12 17:19:47 -0300

    NRU implementado de acordo com o livro

--- lab_mem/page_replacement/python/NRUPolicy.py
+++ lab_mem/page_replacement/python/NRUPolicy.py
@@ -1,12 +1,57 @@
 class NRUPolicy:
+    def __init__(self):
+        self.list = []
+
     def put(self, frameId):
-        pass
+        frame = {'isRead': False, 'isWrite': False, 'frameId': frameId}
+        self.list.append(frame)
+
 
     def evict(self):
-        return 0
+        wasRemoved = False
+        frame_id = -1
+        for frame in self.list:
+            if ((not frame['isRead']) & (not frame['isWrite'])):
+                frame_id = frame['frameId']
+                self.list.remove(frame)
+                wasRemoved = True
+                break
+
+        if (not wasRemoved):    
+            for frame in self.list:
+                if ((not frame['isRead']) & frame['isWrite']):
+                    frame_id = frame['frameId']
+                    self.list.remove(frame)
+                    wasRemoved = True
+                    break
+        if (not wasRemoved):
+            for frame in self.list:
+                if (frame['isRead'] & (not frame['isWrite'])):
+                    frame_id = frame['frameId']
+                    self.list.remove(frame)
+                    wasRemoved = True
+                    break
+        if (not wasRemoved):    
+            for frame in self.list:
+                if (frame['isRead'] & frame['isWrite']):
+                    frame_id = frame['frameId']
+                    self.list.remove(frame)
+                    break
+
+        return frame_id
+
 
     def clock(self):
-        pass
+        for frame in self.list:
+            if frame['isRead'] == True:
+                frame['isRead'] = False
 
     def access(self, frameId, isWrite):
-        pass
+        for frame in self.list:
+            if(frameId == frame['frameId']):
+                frame['isRead'] = True
+                if (isWrite):
+                    frame['isWrite'] = True
+
+
+
