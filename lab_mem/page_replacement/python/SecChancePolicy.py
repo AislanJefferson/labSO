af9306a7d66f4e18990e23519535ff900ae47573
Filipe Gomes <filipe.lima@ccc.ufcg.edu.br>
2018-10-12 11:14:30 -0300

    Remoção de comentários desnecessários

--- lab_mem/page_replacement/python/SecChancePolicy.py
+++ lab_mem/page_replacement/python/SecChancePolicy.py
@@ -2,17 +2,14 @@ from FifoPolicy import FifoPolicy
 
 class SecChancePolicy(FifoPolicy):
     def put(self, frameId):
-        """Allocates this frameId for some page"""
         self.fifo.append([False, frameId])
 
     def evict(self):
-        """Deallocates a frame from the physical memory and returns its frameId"""
         leitura,item = self.fifo.pop(0)
         if (not leitura):
             return item
         else:
              self.fifo.append([False,item])
-        """verificar se o self.evict() eh o do FIfoPolicy ou o do SecChancePolicy"""
         return self.evict()
 
     def access(self, frameId, isWrite):
