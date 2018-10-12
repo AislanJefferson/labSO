5f4d00e41a4c06df7032826c6175f0e01bc5a490
Filipe Gomes <filipe.lima@ccc.ufcg.edu.br>
2018-10-12 19:38:53 -0300

    Implementado o AgingPolicy com herança no NRUPolicy.py

--- lab_mem/page_replacement/python/AgingPolicy.py
+++ lab_mem/page_replacement/python/AgingPolicy.py
@@ -1,12 +1,39 @@
-class AgingPolicy:
-    def put(self, frameId):
-        pass
+from NRUPolicy import NRUPolicy
 
+class AgingPolicy(NRUPolicy):
     def evict(self):
-        return 0
+        n = 256
+        frame_to_remove = {}
+        for frame in self.list:
+            count = self.bin_to_dec(frame)
+            if (count < n):
+                n = count
+                frame_to_remove = frame
+                
+        self.list.remove(frame_to_remove)
+        return frame_to_remove['frameId']
 
     def clock(self):
-        pass
+        for frame in self.list:
+            bit = '0'
+            if (frame['isRead']):
+                bit = '1'
+                frame['isRead'] = False
+
+            new_count = frame['count'][0:7]
+            new_count = bit + new_count
+            frame['count'] = new_count
 
     def access(self, frameId, isWrite):
-        pass
+        for frame in self.list:
+            if(frameId == frame['frameId']):
+                frame['isRead'] = True
+
+    def bin_to_dec(self, frame):
+        count_bin = frame['count']
+        count_int = 0
+        for i in range(7, -1, -1):
+            if (count_bin[i] == '1'):
+                count_int += 2 ** (7 - i)
+
+        return count_int
