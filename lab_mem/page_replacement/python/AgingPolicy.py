1ffca8db5a8f21cee430f24caca5635b70dc115b
Filipe Gomes <filipe.lima@ccc.ufcg.edu.br>
2018-10-16 17:11:44 -0300

    Consertado o shift_count() no LCC2 (nao testei)

--- lab_mem/page_replacement/python/AgingPolicy.py
+++ lab_mem/page_replacement/python/AgingPolicy.py
@@ -2,46 +2,35 @@ from NRUPolicy import NRUPolicy
 
 class AgingPolicy(NRUPolicy):
     def evict(self):
-        n = 256
+        n = 256 #count maximo eh 255
         frame_to_remove = {}
         for frame in self.list:
-            count = self.bin_to_dec(frame)
-            if (count < n):
-                n = count
+            
+            if (frame['count'] < n):
+                n = frame['count']
                 frame_to_remove = frame
-                
+
         self.list.remove(frame_to_remove)
         return frame_to_remove['frameId']
 
     def clock(self):
         for frame in self.list:
-            bit = '0'
+            bit = 0
             if (frame['isRead']):
-                bit = '1'
+                bit = 1
                 frame['isRead'] = False
 
-            new_count = frame['count'][0:7]
-            new_count = bit + new_count
-            frame['count'] = new_count
+            frame['count'] = shitf_count(frame['count'], bit)
 
     def access(self, frameId, isWrite):
         for frame in self.list:
             if(frameId == frame['frameId']):
                 frame['isRead'] = True
 
-    def bin_to_dec(self, frame):
-        count_bin = frame['count']
-        count_int = 0
-        for i in range(7, -1, -1):
-            if (count_bin[i] == '1'):
-                count_int += 2 ** (7 - i)
-
-        return count_int
 
     def shitf_count(count, bit):
-        if (count == 0):
-            if (bit == 1):
-                new_count = 128
+        if ((count == 0) & (bit == 1)):        
+            new_count = 128
         else:
             new_count = count * (bit) + (count >> 1)
 
@@ -49,4 +38,11 @@ class AgingPolicy(NRUPolicy):
             new_count = 255
 
         return new_count
+
+    def shitf_count(count, bit):
+        add_bit = bit
+        if (bit == 1):        
+            add_bit = 128
+
+        return add_bit | (count >> 1)
 
